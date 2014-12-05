525e6a1b532b6d2642bf291a42530da87ea9c767
Brah0um <boukoufallah.brahim@gmail.com>
2014-12-05 10:54:12 +0100

    Update the command's status to 'to_delivery' on click

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,15 +4,47 @@
 
 var barmanControllers = angular.module('barmanControllers', []);
 
+var getOrders = function(array) {
+
+};
+
 /* Command List Controller */
 
 barmanControllers.controller('CommandListController', ['$scope', '$http', function($scope, $http) {
+    $scope.orders = [];
 
-    var promise = $http({method: 'GET', url: 'https://lazywaiter.couchappy.com/orders/_design/orders/_view/all'})
-    promise.success(function(data, status, headers, config) {
-        $scope.orders = data.rows;
-    });
-    promise.error(function(data, status, headers, config) {
-        alert("error: Data not found");
-    });
+    var getData = function(array) {
+        var promise = $http({method: 'GET', url: 'https://lazywaiter.couchappy.com/orders/_design/orders/_view/all'})
+        promise.success(function(data, status, headers, config) {
+            // Get orders with "to_prepare" state
+            for(var i = 0, l = data.rows.length; i < l; i++) {
+                if (data.rows[i].value.state === "to_prepare") {
+                    array.push(data.rows[i].value);
+                }
+            }
+        });
+        promise.error(function(data, status, headers, config) {
+            alert("error: Data not found");
+        });
+    };
+
+
+    getData($scope.orders);
+
+    $scope.updateStatusToReady = function(order) {
+        order.state = "to_delivery";
+        //order._deleted = true;
+        var index = $scope.orders.indexOf(order);
+
+        var promise = $http.put('https://lazywaiter.couchappy.com/orders/' + order._id, order);
+        promise.success(function(data, status, headers, config) {
+            alert("state changed");
+            if (index !== -1) {
+                delete $scope.orders[index];
+            }
+        });
+        promise.error(function(data, status, headers, config) {
+            alert("error: Data not updated");
+        });
+    };
 }]);
