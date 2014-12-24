eea94726208da873b676aae8618d5019bbc0a782
Brah0um <boukoufallah.brahim@gmail.com>
2014-12-24 15:12:29 +0100

    Fix pluralization bug

--- app/js/directives.js
+++ app/js/directives.js
@@ -6,29 +6,17 @@ barmanDirectives
             scope: {},
             controller: ['$scope', '$timeout', function($scope, $timeout) {
                 $scope.counter = 0;
-                $scope.minutes = "minute";
-
-                /*
-                 * Pluralize a word according to the value of counter
-                 * @param counter: The counter which we compare
-                 * @param string: The word which we want to pluralize
-                 *
-                 * @return singular - plural
-                 */
-                var pluralize = function(counter, string) {
-                    return (counter === 1) ? string : string + 's';
-                };
-
-                var mytimeout = $timeout($scope.onTimeout, 60000);
 
                 /*
                  * The timer
                  */
                 $scope.onTimeout = function(){
                     $scope.counter += 1;
-                    $scope.minutes = pluralize($scope.counter, $scope.minutes);
+                    $scope.pluralize =  ($scope.counter === 1) ? '' : 's';
                     mytimeout = $timeout($scope.onTimeout, 60000);
                 };
+
+                var mytimeout = $timeout($scope.onTimeout, 60000);
             }],
             templateUrl: "partials/ordered-at.html"
         }
