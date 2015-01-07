b716480471c3a85004cffb265ca5e4fc03690c67
Brah0um <boukoufallah.brahim@gmail.com>
2015-01-07 21:07:04 +0100

    Post correctly an order in database

--- app/js/services.js
+++ app/js/services.js
@@ -5,6 +5,7 @@
 var barmanServices = angular.module('barmanServices', []);
 
 barmanServices.factory('Order', [ '$http', '$interval', function ($http, $interval) {
+
     /*
      * Check if an order already exists in the scope array
      * @param array: The scope array
@@ -24,40 +25,35 @@ barmanServices.factory('Order', [ '$http', '$interval', function ($http, $interv
 
     return {
         orders : [],
+
+        /*
+         * Insert a new order in database
+         */
         insertOrder: function() {
-            var self = this;
-            var order = {
-                value: {
-                    "tableNumber" : 88, // Number of the table
-                    "createdAt" : new Date().getTime(), // date creation of the command
-                    "products" : [ // all product in the command
-                        {
-                            "name" : "Beer", // name of the product
-                            "quantity" : "1", // quantity for the product
-                            "price" : "3" //price of the product
-                        }
-                    ],
-                    "status" : "to_prepare" // command status
-                }
+            var order =  {
+                "tableNumber" : 88, // Number of the table
+                "createdAt" : new Date().getTime(), // date creation of the command
+                "products" : [ // all product in the command
+                    {
+                        "name" : "Beer", // name of the product
+                        "quantity" : "1", // quantity for the product
+                        "price" : "3" //price of the product
+                    }
+                ],
+                "status" : "to_prepare" // command status
             };
 
-            var promise = $http.post('https://lazywaiter.couchappy.com/orders/', order);
-
-            promise.success(function(data, status, headers, config) {
-                self.orders.push(order);
-            });
-            promise.error(function(data, status, headers, config) {
-                alert("Error on POST Request");
-            });
+            $http.post('https://lazywaiter.couchappy.com/orders/', order);
         },
+
         /*
          * Get the orders from couchDB
          *
          * @param array: it's where we push the data
          */
         fetchOrders: function () {
-            var promise = $http.get('https://lazywaiter.couchappy.com/orders/_design/orders/_view/all');
             var self = this;
+            var promise = $http.get('https://lazywaiter.couchappy.com/orders/_design/orders/_view/all');
 
             promise.success(function(data, status, headers, config) {
                 for (var i = 0, l = data.rows.length; i < l; i++) {
@@ -73,33 +69,39 @@ barmanServices.factory('Order', [ '$http', '$interval', function ($http, $interv
                 alert("error: Data not found");
             });
         },
+
         /*
          * Update status of an order
          *
          * @param $scope: The scope which we modify
          * @param order: The order which want to update the status
          */
-        updateOrderStatusToReady: function(order) {
+        updateStatusToReady: function(order) {
+            var self = this;
             var index = this.orders.indexOf(order);
+            order.value.status = "to_delivery";
 
-            var promise = $http.put('https://lazywaiter.couchappy.com/orders/' + order._id, order);
+            var promise = $http.put('https://lazywaiter.couchappy.com/orders/' + order.id, order.value);
             promise.success(function(data, status, headers, config) {
                 if (index !== -1) {
-                    delete $scope.orders[index];
+                    self.orders.splice(index, 1);
                 }
             });
 
             promise.error(function(data, status, headers, config) {
-                order.status = "to_prepare";
+                order.value.status = "to_prepare";
                 alert("error: Data not updated");
             });
         },
+
+        /*
+         * Refresh data every @param seconds
+         *
+         * @param time: the time which want to set
+         */
         startFetchingEvery: function(time) {
-            /*
-             * Refresh data every @time seconds
-             *
-             */
             var self = this;
+
             $interval(function() {
                 self.fetchOrders();
             }, time);
@@ -110,6 +112,7 @@ barmanServices.factory('Order', [ '$http', '$interval', function ($http, $interv
 barmanServices.factory("ControlTime", [function() {
 
     return {
+
         /*
          * get the order's date until now in minutes
          * @param: The order which want to get the date
